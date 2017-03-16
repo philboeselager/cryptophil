496ebd861815ca027816ed782d103241dcd1fbb7
Philipp von Böselager <philipp.boeselager@web.de>
2017-03-17 00:16:57 +0100

    Sketch FakeCharSet

--- src/main/java/containers/FakeCharSet.java
+++ src/main/java/containers/FakeCharSet.java
@@ -1,5 +1,24 @@
 package containers;
 
+import models.SubstantialChar;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class FakeCharSet implements CharSet{
 
+    final private Map<Character, SubstantialChar> mFakeCharSet = new HashMap<>();
+
+    @Override
+    public String getEncoded(Character aChar) {
+        if (!mFakeCharSet.containsKey(aChar)){
+            mFakeCharSet.get(aChar).getEncoded();
+        }
+        return mFakeCharSet.get(aChar).getEncoded();
+    }
+
+    @Override
+    public Character getDecoded(String aEncoded) {
+        return null;
+    }
 }
